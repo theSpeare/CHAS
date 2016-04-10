f5000be01527ddce7506a4aca346f55a359f47b1
Lui Villarias <luivillarias@gmail.com>
2016-04-11 11:05:21 +1200

    initial commit
    work in progress

---
+++ _GM_AdvisorCat.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class _GM_AdvisorCat : MonoBehaviour {
+	public Text acd_textbox;
+	public GameObject dialogWindow;
+	public _currentDialog currentDialog = new _currentDialog();
+	public _masterDialog masterDialog = new _masterDialog();
+
+	public void Start () {
+		//currentDialog.n = 0;
+		hideDialog();
+		//currentDialog.lines = masterDialog._masterDialogDict["intro"];
+	}
+
+	public void nextLine(){
+		if (currentDialog.n >= currentDialog.lines.Count-1) {
+			hideDialog ();
+		} else {
+			currentDialog.n++;
+			acd_textbox.text = currentDialog.lines [currentDialog.n];
+		}
+	}
+		
+	public void showDialog(string nameOfLines){
+		currentDialog.n = 0;
+		currentDialog.lines = masterDialog._masterDialogDict [nameOfLines]; //find dialog inputted and set current dialog
+		acd_textbox.text = currentDialog.lines[currentDialog.n]; //show lines in current dialog
+
+		dialogWindow.SetActive (true);
+	}
+
+	public void hideDialog(){
+		dialogWindow.SetActive (false);
+	}
+}
+
+public class _currentDialog {
+	public List<string> lines = new List<string>();
+	public int n = 0;
+}
+
+public class _masterDialog {
+	public int n = 0;
+	public Dictionary<string, List<string>> _masterDialogDict = new Dictionary<string, List<string>>();
+
+	public _masterDialog () {
+		_masterDialogDict.Add ("intro", new List<string>()
+			{
+				"Hello! I am your advisor cat.",
+				"This is your cat hospital.",
+				"No, no, no, not a hospital FOR cats",
+				"It's a hospital run BY cats."
+			}
+		);
+
+		_masterDialogDict.Add ("test2", new List<string>()
+			{
+				"Hey!",
+				"I want to suck your blood!",
+				"--Dick!"
+			}
+		);
+
+	}
+}
---
+++ gm.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class gm : MonoBehaviour {
+	public Text moneyDisplay;
+	public int money = 0;
+
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad (this);
+		//_GM_AdvisorCat AC = gameObject.GetComponent<_GM_AdvisorCat> ();
+		//AC.showDialog ();
+		//AC.message = "world!";
+	}
+	// Update is called once per frame
+
+	public void tempEarnMoney (int earn){
+		money += earn;
+	}
+
+	public void UpdateUI() {
+		moneyDisplay.text = money.ToString ();
+	}
+
+	void Update () {
+	}
+}
